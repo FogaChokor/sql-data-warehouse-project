a581ec3b5224386405b20a7ed398a8426046464a
Foga <56896843+FogaChokor@users.noreply.github.com>
2026-08-14 13:50:45 +0100

    Update stored_procedure_load_bronze_layer.sql

--- scripts/bronze_layer/stored_procedure_load_bronze_layer.sql
+++ scripts/bronze_layer/stored_procedure_load_bronze_layer.sql
@@ -52,7 +52,7 @@ BEGIN
 
 			PRINT 'Inserting Data Into: bronze.crm_prd_info';
 			BULK INSERT bronze.crm_prd_info
-			FROM 'C:\Users\fogac\Documents\MyLearning\Projects\SQL Data Warehouse Project\datasets\source_crm\prd_info.csv'
+			FROM 'C:\Users\fogac\Documents\Projects\SQL Data Warehouse Project\datasets\source_crm\prd_info.csv'
 			WITH (
 				FIRSTROW = 2,
 				FIELDTERMINATOR = ',',
@@ -68,7 +68,7 @@ BEGIN
 
 			PRINT 'Inserting Data Into: bronze.crm_sales_details';
 			BULK INSERT bronze.crm_sales_details
-			FROM 'C:\Users\fogac\Documents\MyLearning\Projects\SQL Data Warehouse Project\datasets\source_crm\sales_details.csv'
+			FROM 'C:\Users\fogac\Documents\Projects\SQL Data Warehouse Project\datasets\source_crm\sales_details.csv'
 			WITH (
 				FIRSTROW = 2,
 				FIELDTERMINATOR = ',',
@@ -90,7 +90,7 @@ BEGIN
 
 			PRINT 'Inserting Data Into: bronze.erp_cust_az12';
 			BULK INSERT bronze.erp_cust_az12
-			FROM 'C:\Users\fogac\Documents\MyLearning\Projects\SQL Data Warehouse Project\datasets\source_erp\cust_az12.csv'
+			FROM 'C:\Users\fogac\Documents\Projects\SQL Data Warehouse Project\datasets\source_erp\cust_az12.csv'
 			WITH (
 				FIRSTROW = 2,
 				FIELDTERMINATOR = ',',
@@ -106,7 +106,7 @@ BEGIN
 
 			PRINT 'Inserting Data Into: bronze.erp_loc_a101';
 			BULK INSERT bronze.erp_loc_a101
-			FROM 'C:\Users\fogac\Documents\MyLearning\Projects\SQL Data Warehouse Project\datasets\source_erp\loc_a101.csv'
+			FROM 'C:\Users\fogac\Documents\Projects\SQL Data Warehouse Project\datasets\source_erp\loc_a101.csv'
 			WITH (
 				FIRSTROW = 2,
 				FIELDTERMINATOR = ',',
@@ -122,7 +122,7 @@ BEGIN
 
 			PRINT 'Inserting Data Into: bronze.erp_px_cat_g1v2';
 			BULK INSERT bronze.erp_px_cat_g1v2
-			FROM 'C:\Users\fogac\Documents\MyLearning\Projects\SQL Data Warehouse Project\datasets\source_erp\px_cat_g1v2.csv'
+			FROM 'C:\Users\fogac\Documents\Projects\SQL Data Warehouse Project\datasets\source_erp\px_cat_g1v2.csv'
 			WITH (
 				FIRSTROW = 2,
 				FIELDTERMINATOR = ',',
